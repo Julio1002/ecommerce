0bd8ce16312d5575283330bdf10beb8ab2baa560
Julio Silva <juliocsilvapdr@gmail.com>
2017-09-29 01:16:07 -0300

    Erro nos arquivos

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitaee5b5bb6fac70230afeb4a920f357f8
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInitaee5b5bb6fac70230afeb4a920f357f8
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInitaee5b5bb6fac70230afeb4a920f357f8::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitaee5b5bb6fac70230afeb4a920f357f8::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitaee5b5bb6fac70230afeb4a920f357f8::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInitaee5b5bb6fac70230afeb4a920f357f8::$prefixesPsr0;
             $loader->classMap = ComposerStaticInitaee5b5bb6fac70230afeb4a920f357f8::$classMap;
 
